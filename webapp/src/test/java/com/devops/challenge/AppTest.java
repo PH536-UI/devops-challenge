a8687e9cf54ce565c5ece7946776bd7bff8ef44d
PH536-UI <Paulohenrique2020pereira@gmail.com>
2026-06-09 01:47:41 +0000

    feat: formalize codebuild pipeline with versioned artifacts - day 4

--- webapp/src/test/java/com/devops/challenge/AppTest.java
+++ webapp/src/test/java/com/devops/challenge/AppTest.java
@@ -1,20 +1,27 @@
 package com.devops.challenge;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAppExists() {
+        App app = new App();
+        assertNotNull(app);
+    }
+
+    @Test
+    public void testGreeting() {
+        String greeting = "Hello DevOps!";
+        assertEquals("Hello DevOps!", greeting);
+        System.out.println("Test passou: " + greeting);
+    }
+
+    @Test
+    public void testBuildVersion() {
+        String version = "1.0-SNAPSHOT";
+        assertTrue(version.contains("SNAPSHOT"));
+        System.out.println("Versao do build: " + version);
     }
 }
